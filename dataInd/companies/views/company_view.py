cdc41a42c8a9c3793d6ddce519456556ec732e67
Edisonnarvaez <edison.narvaez@campusucc.edu.co>
2024-10-04 15:39:47 -0500

    correcciones apis

--- dataInd/companies/views/company_view.py
+++ dataInd/companies/views/company_view.py
@@ -2,6 +2,8 @@ from companies.models.company import Company
 from companies.serializers.company_serializer import CompanySerializer
 from rest_framework import viewsets
 from rest_framework.decorators import action
+from rest_framework.response import Response
+
 
 # ViewSet para Company
 class CompanyViewSet(viewsets.ModelViewSet):
--- dataInd/users/models/user.py
+++ dataInd/users/models/user.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 from companies.models.company import Company
 from companies.models.department import Department
@@ -12,14 +12,21 @@ class UserManager(BaseUserManager):
         user.set_password(password)
         user.save(using=self._db)
         return user
-
+ 
     def create_superuser(self, username, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
+        if 'companyId' not in extra_fields:
+            extra_fields['companyId'] = Company.objects.first()  # Asigna un valor por defecto
+        if 'departmentId' not in extra_fields:
+            extra_fields['departmentId'] = Department.objects.first()  # Asigna un valor por defecto
+        if 'role' not in extra_fields:
+            extra_fields['role'] = Role.objects.first()  # Asigna un valor por defecto
+
         return self.create_user(username, password, **extra_fields)
 
-class User(AbstractBaseUser):
+class User(AbstractBaseUser, PermissionsMixin):  # Agrega PermissionsMixin para soporte de permisos
     userId = models.CharField(max_length=100, primary_key=True)  # Clave primaria personalizada
     firstName = models.CharField(max_length=255)
     lastName = models.CharField(max_length=255)
@@ -39,10 +46,19 @@ class User(AbstractBaseUser):
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
 
-    USERNAME_FIELD = 'username'  # Define el campo usado para el login
-    REQUIRED_FIELDS = ['email', 'firstName', 'lastName']  # Otros campos requeridos
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email', 'firstName', 'lastName']
 
-    objects = UserManager()  # Asigna el manager personalizado
+    objects = UserManager()
 
     def __str__(self):
         return self.username
+
+    # Métodos de permisos
+    def has_perm(self, perm, obj=None):
+        """Este método verifica si el usuario tiene un permiso específico."""
+        return self.is_superuser or self.is_active
+
+    def has_module_perms(self, app_label):
+        """Este método verifica si el usuario tiene permiso para ver el módulo (aplicación) especificado."""
+        return self.is_superuser or self.is_active
