683596b075a5e077f49198261570499bf7eab6db
Edison Stiven Narvaez Paz <124762853+Edisonnarvaez@users.noreply.github.com>
2025-05-13 17:02:20 -0500

    Update settings.py

--- dataInd/dataInd/settings.py
+++ dataInd/dataInd/settings.py
@@ -139,6 +139,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',  # URL donde corre Vite
     'http://127.0.0.1:5173', # URL donde corre Vite
+    'https://dataind.up.railway.app',
 ]
 
 REST_FRAMEWORK = {
