c6883edc22a4417be3299eb7c98aa5954fa67a18
Edison Stiven Narvaez Paz <124762853+Edisonnarvaez@users.noreply.github.com>
2025-05-14 13:28:03 -0500

    Update settings.py

--- dataInd/dataInd/settings.py
+++ dataInd/dataInd/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users',
+    'users.apps.UsersConfig',
     'companies',
     'indicators',
     'rest_framework',
